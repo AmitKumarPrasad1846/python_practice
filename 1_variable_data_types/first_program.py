3c7c4d52a6ca4a94900c2b48d43e84c465833a7c
Amit Kumar Prasad <amitkrprasad1846@gmail.com>
2026-06-13 20:06:36 +0530

    first commit

---
+++ 1_variable_data_types/first_program.py
@@ -0,0 +1,10 @@
+# save python code with ".py" extension.
+print("hello world")
+# here "print" is function
+
+# PYTHON CHARACTER SET
+# letters - A to Z, a to z
+# Digits - 0 - 9
+# Special Symbols - + * / etc.
+# Whitespaces - Blank Space, tab, carriage return, newline, formfeed
+# Other characters - Python can process all ASCII and Unicode characters as part of data or literals.
---
+++ 1_variable_data_types/variables.py
@@ -0,0 +1,5 @@
+print("hello world", "I am Amit Kumar Prasad")
+print(23)
+print(23 + 32)
+
+# VARIABLES
