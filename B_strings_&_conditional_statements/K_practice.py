b0db647bdfa526ec68269c23c777f2944a65de1c
Amit Kumar Prasad <amitkrprasad1846@gmail.com>
2026-06-15 16:00:16 +0530

    modefied k

--- B_strings_&_conditional_statements/K_practice.py
+++ B_strings_&_conditional_statements/K_practice.py
@@ -1,3 +1,7 @@
 # WAP to check if a number is a multiple of 7 or not.
 
 num = int(input("enter the no which you want to check :"))
+if num%7==0:
+    print(num, "is divisible by 7")
+else:
+    print(num, "is not divisible by 7")
